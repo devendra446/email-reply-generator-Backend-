e5ec3cff3d86f929910cbdfcd9737069da223672
Devendra <rahullokesh786@gmail.com>
2025-07-29 17:20:31 +0530

    Initial commit

---
+++ src/main/java/com/email/email_writer_sb/app/EmailRequest.java
@@ -0,0 +1,14 @@
+package com.email.email_writer_sb.app;
+
+public class EmailRequest {
+    private String tone;
+    private String emailContent;
+
+    public String getTone() {
+        return tone;
+    }
+
+    public String getEmailContent() {
+        return emailContent;
+    }
+}
